4c788b7d014325bbb2f1d49ac39abc81afeea846
shawyunz <shawyunz@gmail.com>
2022-08-23 16:56:59 +1200

    Four cards stacked and add velocity calculation.

--- BoomerangCard.Maui/MainPage.xaml.cs
+++ BoomerangCard.Maui/MainPage.xaml.cs
@@ -2,9 +2,11 @@
 
 public partial class MainPage : ContentPage
 {
-    private bool isflying = false;
-
-    private View topview;
+    private bool _isflying = false;
+    private bool _isTravelUp;
+    private View _topview;
+    private double _travelDistance;
+    private DateTime _travelStart;
 
     public MainPage()
     {
@@ -17,106 +19,130 @@ public partial class MainPage : ContentPage
 
     private async void Button_Clicked(object sender, EventArgs e)
     {
-        await FlyBoomerang();
-
-        isflying = false;
-    }
-
-    private async Task FlyBoomerang()
-    {
-        isflying = true;
-        var viewList = GetCardList();
-
-        var view1 = viewList[0];
-        var view2 = viewList[1];
-
-        view1.RelRotateTo(AniSpins * 360, 800);
-        view1.ScaleTo(0.95, 800);
-
-        view2.ScaleTo(1, 200);
-        view2.TranslateTo(0, 10, 200);
-
-        await view1.TranslateTo(0, -AniHeight, 400, Easing.CubicOut);
-        //gridbox.LowerChild(view1);
-        view2.ZIndex = 2;
-        view1.ZIndex = 1;
-        view1.Margin = new Thickness(0, 0, 0, 20);
-        await view1.TranslateTo(0, 0, 400, Easing.CubicIn);
-
-        view1.Scale = 0.95;
-        view2.Scale = 1;
-    }
-
-    private List<View> GetCardList()
-    {
-        var cardList = new List<View>();
-        if (card1.Scale == 1)
-        {
-            cardList.Add(card1);
-            cardList.Add(card2);
-        }
-        else
-        {
-            cardList.Add(card2);
-            cardList.Add(card1);
-        }
-
-        return cardList;
-    }
-
-    private async void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
-    {
-        if (isflying)
+        if (_isflying)
         {
             return;
         }
 
-        if (topview == null)
+        LabelStatus.Text = "Status: Flying";
+        LabelPower.Text = "Power: As slider below";
+
+        await FlyBoomerang(AniSpins, AniHeight);
+
+        _isflying = false;
+    }
+
+    private async Task FlyBoomerang(int spins, double height)
+    {
+        _isflying = true;
+
+        var cardList = gridbox.Children.Cast<CardView>().OrderBy(x => x.ZIndex).ToList();
+        var topCardIndex = cardList.Count - 1;
+
+        //fly starts
+        var viewFly = cardList[topCardIndex];
+        //viewFly.RotateTo(spins * (isRightSide ? -360 : 360), 800);
+        viewFly.RelRotateTo(spins * 360, 800);
+        viewFly.ScaleTo(1 - 0.05 * topCardIndex, 800);
+
+        for (int i = 0; i < topCardIndex; i++)
         {
-            topview = GetCardList()[0];
+            cardList[i].ScaleTo(cardList[i].Scale + 0.05, 200);
+            cardList[i].TranslateTo(0, 10, 200);
+            cardList[i].ZIndex = i + 2;
         }
 
-        var offset = e.TotalY;
-        LabelStatus.Text = "Status: " + e.StatusType.ToString();
-        //LabelY.Text = "Movement: " + Math.Round(e.TotalY, 1);
+        await viewFly.TranslateTo(0, -height, 400, Easing.CubicOut);
+        viewFly.ZIndex = 1;
+        viewFly.Margin = new Thickness(0, 0, 0, 20 * topCardIndex);
+        await viewFly.TranslateTo(0, 0, 400, Easing.CubicIn);
+
+        //refresh positions when fly ends
+        for (int i = 0; i < topCardIndex; i++)
+        {
+            cardList[i].TranslationY = 0;
+            cardList[i].Margin = new Thickness(0, 0, 0, cardList[i].Margin.Bottom - 20);
+        }
+    }
+
+    private double GetFlyCapacity(double velocity)
+    {
+        // v < 0.5 means least spin, which is 0.1
+        // v > 1.5 means 100%
+        return (velocity < 0.5 ? 0 : (velocity > 1.5 ? 0.9 : ((velocity - 0.5) * 0.9))) + 0.1;
+    }
+
+    private async void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
+    {
+        if (_isflying)
+        {
+            return;
+        }
 
         switch (e.StatusType)
         {
             case GestureStatus.Started:
-                if (topview.Rotation != -10)
+                if (_topview == null)
                 {
-                    topview.Rotation = -10;
+                    _topview = sender as View;
+                }
+
+                if (_topview.Rotation != -10)
+                {
+                    _topview.Rotation = -10;
                 }
                 break;
 
             case GestureStatus.Running:
-                topview.TranslationX += e.TotalX;
-                topview.TranslationY += offset;
-                break;
+                var offset = e.TotalY;
+                LabelStatus.Text = "Status: " + e.StatusType.ToString();
+                //LabelY.Text = "Movement: " + Math.Round(e.TotalY, 1);
 
-            case GestureStatus.Completed:
-                if (topview.TranslationY < 0)
+                if (offset > 0)
                 {
-                    await FlyBoomerang();
-                }
-                else if (topview.TranslationY > 100)
-                {
-                    AniHeight = 350;
-                    AniSpins = 6;
-                    await FlyBoomerang();
-                    AniHeight = 240;
-                    AniSpins = 1;
+                    _travelDistance = 0;
+                    _isTravelUp = false;
                 }
                 else
                 {
-                    topview.TranslationX = 0;
-                    topview.TranslationY = 10;
+                    if (!_isTravelUp)
+                    {
+                        _travelStart = DateTime.Now;
+                        _isTravelUp = true;
+                    }
+                    _travelDistance -= offset;
                 }
 
-                topview.Rotation = 0;
-                topview = null;
+                _topview.TranslationX += e.TotalX;
+                _topview.TranslationY += offset;
+                break;
 
-                isflying = false;
+            case GestureStatus.Completed:
+                if (_isTravelUp && _topview.TranslationY < 0)
+                {
+                    //have to throw higher than original position to fly
+                    var portion = GetFlyCapacity(_travelDistance / (int)DateTime.Now.Subtract(_travelStart).TotalMilliseconds);
+                    LabelPower.Text = $"Power: {portion:0 %}";
+                    await FlyBoomerang((int)(10 * portion), _travelDistance + 300 * portion + 100);
+                }
+                else if (_topview.TranslationY > 100)
+                {
+                    LabelPower.Text = "Power: 100%";
+                    await FlyBoomerang(10, 450);
+                }
+                else
+                {
+                    _topview.TranslationX = 0;
+                    _topview.TranslationY = 0;
+                }
+
+                _topview.Rotation = 0;
+                LabelPower.Text = "Power: 00%";
+                _topview = null;
+
+                _travelDistance = 0;
+                _isTravelUp = false;
+                _isflying = false;
                 break;
         }
     }
